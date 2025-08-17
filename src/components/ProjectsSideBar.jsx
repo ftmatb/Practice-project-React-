228728510a0e67c488316669995587f0a332478d
Sofi9371 <146138151+ftmatb@users.noreply.github.com>
2025-08-17 10:47:00 +0200

    styling buttons and input

--- src/components/ProjectsSideBar.jsx
+++ src/components/ProjectsSideBar.jsx
@@ -1,11 +1,11 @@
 export default function ProjectsSideBar() {
   return (
-    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
-      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+    <aside className="w-1/3 px-8 py-16 bg-red-900 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-red-200">
         Your Projects
       </h2>
       <div>
-        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-red-700 text-red-400 hover:bg-red-600 hover:text-red-100">
           + Add Project
         </button>
       </div>
