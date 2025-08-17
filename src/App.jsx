34ea326cfa4dcc5c7c458aba9f51554864a5b448
Sofi9371 <146138151+ftmatb@users.noreply.github.com>
2025-08-17 16:37:50 +0200

    activate "New project" Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSideBar from "./components/ProjectsSideBar";
 
 function App() {
+  const [ProjectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartedAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (ProjectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (ProjectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartedAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar />
-      <NoProjectSelected />
+      <ProjectsSideBar onStartAddProject={handleStartedAddProject} />
+      {content}
+      
     </main>
   );
 }
